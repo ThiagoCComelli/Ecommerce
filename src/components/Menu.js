1c8cc7a3f9e6e60c7a559d80e864d453546eea02
ThiagoCComelli <thiago.comelli@outlook.com>
2020-12-02 22:16:52 -0300

    menu items

--- src/components/Menu.js
+++ src/components/Menu.js
@@ -1,10 +1,10 @@
 import React,{useContext} from 'react'
 import UserContext from '../middleware/context'
-
+import MenuItem from './MenuItem'
 import '../styles/Menu.css'
 
-function Menu(props){
-    // const {userData,setUserData} = useContext(UserContext)
+function Menu(){
+    const {userData,setUserData} = useContext(UserContext)
 
     return(
         <>
@@ -12,8 +12,20 @@ function Menu(props){
                 <div className="mainMenuCircle"></div>
                 <div className="mainMenuContents">
                     <div className="mainMenuContentsItems">
-
+                        <MenuItem price="49.90" ingredients="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris gravida." description="Lorem ipsum \n dolor sit amet, consectetur adipiscing elit. Integer tincidunt pulvinar nisl, sed tempus risus tincidunt ut. Donec euismod augue vitae leo venenatis, et tincidunt dui porta." title="Lorem ipsum dolor." image="pizzaMenu.jpg"/>
+                        <MenuItem price="49.90" ingredients="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris gravida." description="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer tincidunt pulvinar nisl, sed tempus risus tincidunt ut. Donec euismod augue vitae leo venenatis, et tincidunt dui porta." title="Lorem ipsum dolor." image="pizzaMenu.jpg"/>
+                        <MenuItem price="49.90" ingredients="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris gravida." description="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer tincidunt pulvinar nisl, sed tempus risus tincidunt ut. Donec euismod augue vitae leo venenatis, et tincidunt dui porta." title="Lorem ipsum dolor." image="pizzaMenu.jpg"/>
+                        <MenuItem price="49.90" ingredients="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris gravida." description="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer tincidunt pulvinar nisl, sed tempus risus tincidunt ut. Donec euismod augue vitae leo venenatis, et tincidunt dui porta." title="Lorem ipsum dolor." image="pizzaMenu.jpg"/>
+                        <MenuItem price="49.90" ingredients="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris gravida." description="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer tincidunt pulvinar nisl, sed tempus risus tincidunt ut. Donec euismod augue vitae leo venenatis, et tincidunt dui porta." title="Lorem ipsum dolor." image="pizzaMenu.jpg"/>
+                        <MenuItem price="49.90" ingredients="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris gravida." description="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer tincidunt pulvinar nisl, sed tempus risus tincidunt ut. Donec euismod augue vitae leo venenatis, et tincidunt dui porta." title="Lorem ipsum dolor." image="pizzaMenu.jpg"/>
+                        <MenuItem price="49.90" ingredients="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris gravida." description="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer tincidunt pulvinar nisl, sed tempus risus tincidunt ut. Donec euismod augue vitae leo venenatis, et tincidunt dui porta." title="Lorem ipsum dolor." image="pizzaMenu.jpg"/>
                     </div>
+                </div>
+                <div onClick={() => {
+                        setUserData({...userData,'actualScreen':'mainHero','lastScreen':userData['actualScreen']})
+                    }} className="mainMenuLeftArrow">
+                    <img alt="Return button" src={`${process.env.PUBLIC_URL}/images/leftArrow.svg`}></img>
+                    Voltar
                 </div>
             </div>
         </>
---
+++ src/components/MenuItem.js
@@ -0,0 +1,37 @@
+import React,{useState} from 'react'
+
+import '../styles/MenuItem.css'
+
+function MenuItem(props){
+    const [infos,setInfos] = useState({quantity:0,image:props.image,title:props.title,description:props.description,ingredients:props.ingredients,price:props.price})
+    
+    return(
+        <>
+            <div className="menuItemMain">
+                <div className="menuItemContents">
+                    <div className="menuItemImage">
+                        <img alt="Product image" src={`${process.env.PUBLIC_URL}/images/${infos.image}`}></img>                   
+                    </div>
+                    <div className="menuItemDescContents">
+                        <h2>{infos.title}</h2>
+                        <p>{infos.description}</p>
+                        <span>{infos.ingredients}</span>
+                    </div>
+                    <div className="menuItemPrice">
+                        <strong>R${infos.price}</strong>
+                    </div>
+                    <div className="menuItemBtns">
+                        <span>{infos.quantity !== 0 ? `Quantidade: ${infos.quantity}` : false}</span>
+                        <button onClick={() => {setInfos({...infos,quantity:infos.quantity+1})}}>Adicionar</button>
+                        <button onClick={() => {
+                            if(infos.quantity > 0){
+                                setInfos({...infos,quantity:infos.quantity-1})}
+                            }}>Remover</button>
+                    </div>
+                </div>
+            </div>
+        </>
+    )
+}
+
+export default MenuItem
